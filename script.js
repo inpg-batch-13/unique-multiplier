966b6c899f66f000e9999aa33afa7605847ca8e1
Rahmat Hidayat <kickyou44@gmail.com>
2019-05-24 20:27:19 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function multiply(arr) {
+  // your code here
+}
+
+function uniqueMultiplier(arr) {
+  // your code here
+}
+
+// TEST CASES
+console.log(uniqueMultiplier([2, 4, 6])); // [24, 12, 8]
+console.log(uniqueMultiplier([1, 2, 3, 4, 5])); // [120, 60, 40, 30, 24]
+console.log(uniqueMultiplier([1, 4, 3, 2, 5])); // [120, 30, 40, 60, 24]
+console.log(uniqueMultiplier([1, 3, 3, 1])); // [9, 3, 3, 9]
+console.log(uniqueMultiplier([2, 1, 8, 10, 2])); // [160, 320, 40, 32, 160]
